cc1197b50458ed9e40aca7743da9933b4b63c1be
idahir <idahir2000@gmail.com>
2014-02-23 15:01:05 -0600

    First Commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RMSAcademy.Startup))]
+namespace RMSAcademy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
